4a50e9a09bb74756201e80c65e79b381fc0e2cde
ryo <ba068082@gmail.com>
2019-05-27 08:16:00 +0900

    [from now] 2019/05/27 08:16:00
    diff --git a/src/camelConverter.ts b/src/camelConverter.ts
    index 463f4f3..e50abfa 100644
    --- a/src/camelConverter.ts
    +++ b/src/camelConverter.ts
    @@ -1,24 +1,16 @@
     class CamelConverter {
    -  static convert<T, K>(obj): K {
    +  static convert<K>(obj): K {
         return Object.keys(obj).reduce((acc, k) => {
    -      if(typeof obj[k] === 'number') {
    -        acc[this.toCamel(k)] = obj[k] || 0;
    -        return acc
    -      }
    -      if(typeof obj[k] === 'string') {
    -        acc[this.toCamel(k)] = obj[k] || '';
    -        return acc
    -      }
           if(Object.prototype.toString.call(obj[k]) === 'object') {
    -        acc[this.toCamel(k)] = this.convert(obj[k]);
    +        acc[this.snakeToCamel(k)] = this.convert(obj[k]);
             return acc
           }
    -      acc[this.toCamel(k)] = obj[k];
    +      acc[this.snakeToCamel(k)] = obj[k];
           return acc
         }, {} as K);
       };
    
    -  static toCamel(str: string): string {
    +  static snakeToCamel(str: string): string {
         return str.replace(/_(\w)/g, (_capture, match: string) => {
           return match.toUpperCase()
         });

--- src/camelConverter.ts
+++ src/camelConverter.ts
@@ -1,24 +1,16 @@
 class CamelConverter {
-  static convert<T, K>(obj): K {
+  static convert<K>(obj): K {
     return Object.keys(obj).reduce((acc, k) => {
-      if(typeof obj[k] === 'number') {
-        acc[this.toCamel(k)] = obj[k] || 0;
-        return acc
-      }
-      if(typeof obj[k] === 'string') {
-        acc[this.toCamel(k)] = obj[k] || '';
-        return acc
-      }
       if(Object.prototype.toString.call(obj[k]) === 'object') {
-        acc[this.toCamel(k)] = this.convert(obj[k]);
+        acc[this.snakeToCamel(k)] = this.convert(obj[k]);
         return acc
       }
-      acc[this.toCamel(k)] = obj[k];
+      acc[this.snakeToCamel(k)] = obj[k];
       return acc
     }, {} as K);
   };
 
-  static toCamel(str: string): string {
+  static snakeToCamel(str: string): string {
     return str.replace(/_(\w)/g, (_capture, match: string) => {
       return match.toUpperCase()
     });
